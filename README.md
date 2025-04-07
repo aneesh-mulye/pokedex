123287d6589c0dc7edce23bace6e66b5579479a8
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2025-04-07 22:24:54 +0530

    Command registry; and help and exit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pokedex
 boot.dev pokedex project
+
+## Notes on various things
+
+Wow, WTF, Golang? I have to do this bizarrely convoluted way of going around
+and updating fields in a map? But more importantly, WTF if up the the circular
+reference logic? There's perfectly valid code that should compile: a map of
+command names to commands structs, wherein each struct has a name, a
+description, and a callback field that's that command's callback (of type
+`func() error`; and one command callback which references this command registry
+map to generate the name-description pairs for display in a `help` command.
+This apparently doesn't work, because why even what?!; WTF, Go?
--- main.go
+++ main.go
@@ -8,20 +8,36 @@ import (
 )
 
 func main() {
+
+	initialiseCommandRegistry()
+
 	prompt := "Pokedex > "
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print(prompt)
 		if !scanner.Scan() {
 			break
 		}
+
 		rawCmd := scanner.Text()
 		parsedCmd := cleanInput(rawCmd)
 		if 0 == len(parsedCmd) {
 			continue
 		}
-		firstWord := parsedCmd[0]
-		fmt.Println("Your command was: " + firstWord)
+
+		cmdWord := parsedCmd[0]
+		cmdFun := commands[cmdWord].callback
+		if nil == cmdFun {
+			fmt.Fprintln(os.Stderr, "Unknown command")
+			continue
+		}
+
+		err := cmdFun()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err.Error())
+			continue
+		}
 	}
 }
 
@@ -31,4 +47,57 @@ func cleanInput(text string) []string {
 	split := strings.Fields(lowercased)
 
 	return split
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    nil,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    nil,
+	},
+}
+
+var commandCallbacks = map[string]func() error{
+	"exit": commandExit,
+	"help": commandHelp,
+}
+
+func initialiseCommandRegistry() {
+	for n, cmd := range commands {
+		wCallback := cliCommand{
+			name:        cmd.name,
+			description: cmd.description,
+			callback:    commandCallbacks[n],
+		}
+		commands[n] = wCallback
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+
+	for _, command := range commands {
+		fmt.Println(command.name + ": " + command.description)
+	}
+
+	return nil
 }
